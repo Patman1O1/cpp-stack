e28050369db17be0ab5117e4a9fcc302d51c8d8b
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 20:56:32 -0500

    Edit stack.hpp

--- include/collections/stack.hpp
+++ include/collections/stack.hpp
@@ -1,4 +1,29 @@
-#ifndef CPP_STACK_HPP
-#define CPP_STACK_HPP
+#ifndef COLLECTIONS_STACK_HPP
+#define COLLECTIONS_STACK_HPP
 
-#endif // #ifndef CPP_STACK_HPP
+// ISO C Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <iterator>
+#include <ranges>
+
+namespace collections {
+    template<typename T, std::ranges::range Container>
+    class stack {
+    public:
+        using value_type = T;
+
+	using container_type = Container;
+
+	using size_type = std::size_t;
+
+	using difference_type = std::ptrdiff_t;
+
+	using reference = value_type&;
+
+	using const_reference = const value_type&;
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_STACK_HPP
